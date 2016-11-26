df2bec97743bfbcfcba069d2d2429ef52b5034d3
Murat Çelik <muratcelik.27@outlook.com>
2016-11-26 20:58:56 +0300

    Entity Model

---
+++ IssueTracker/App_Code/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IssueTracker.App_Code
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public DateTime DateTimeCreate { get; set; }
+        public DateTime DateTimeUpdate { get; set; }
+    }
+}
