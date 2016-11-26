23260b8dd1776c25df32cb01111d7304218bfb7f
Murat Çelik <muratcelik.27@outlook.com>
2016-11-26 21:57:03 +0300

    Entity base Property

---
+++ IssueTracker/AppCode/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IssueTracker.AppCode
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public int UserCreatedId { get; set; }
+        public int UserUpdatedId { get; set; }
+
+        public DateTime DateTimeCreated { get; set; }
+        public DateTime DateTimeUpdated { get; set; }
+    }
+}
--- IssueTracker/Models/Team.cs
+++ IssueTracker/Models/Team.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace IssueTracker.App_Code
+namespace IssueTracker.Models
 {
-    public class IssueTrackerContext
+    public class Team : AppCode.Entity
     {
-       
+
     }
 }
