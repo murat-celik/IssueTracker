4b572bc8cee30c7556c81acf59d45e32d50220a3
Murat Çelik <muratcelik.27@outlook.com>
2016-12-01 00:51:34 +0300

    Qualification Model

--- IssueTracker/Models/Qualification .cs
+++ IssueTracker/Models/Qualification .cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IssueTracker.Models
 {
     public class Qualification : AppCode.BaseEntity
     {
-
+        [Required, StringLength(128), Column(TypeName = "varchar")]
+        public string Name { get; set; }
+        [Column(TypeName = "varchar(MAX)")]
+        public string Desription { get; set; }
     }
 }
