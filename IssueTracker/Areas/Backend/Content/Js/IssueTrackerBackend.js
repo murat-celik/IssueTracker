963856f9e345458cdbe052477b3b383803ac8275
Murat Çelik <muratcelik.27@outlook.com>
2016-12-04 14:36:05 +0300

    Update link

--- IssueTracker/Areas/Backend/Content/Js/IssueTrackerBackend.js
+++ IssueTracker/Areas/Backend/Content/Js/IssueTrackerBackend.js
@@ -1,9 +1,7 @@
 ﻿function managerCreateTeam() {
-
     ajax('Manager/CreateTeam', $('#manager-create-team').serialize(), function (data) {
-        $("#list-group-teams").append('<li class="list-group-item"><a href="Team/Detail/' + data.ID + '">' + data.Name + '</a></li>');
+        $("#list-group-teams").append('<li class="list-group-item"><a href="Team/Details/' + data.ID + '">' + data.Name + '</a></li>');
         $("#team-modal").modal("hide");
         console.log(data);
     }, true, false);
-
 }
